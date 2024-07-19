50922b2f6060e556153f2403f79f15c82f197246
Damian-MG <malenogon_f@hotmail.com>
2024-07-19 20:18:51 +0200

    Parte 2 completada

--- MovilViajero/movilviajero/settings.py
+++ MovilViajero/movilviajero/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # This is where we add new applications
+    'catalogo.apps.CatalogoConfig'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
