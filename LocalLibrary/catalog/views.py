2198a872071e79a619a4538f2cb76a77a471b85d
Damian-MG <malenogon_f@hotmail.com>
2024-07-30 00:41:59 +0200

    6 before accessing variables

--- LocalLibrary/catalog/views.py
+++ LocalLibrary/catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+
 
 # Create your views here.
 def index(request):
@@ -29,3 +31,18 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5]
+    context_object_name = 'my_book_list'   # su propio nombre para la lista como variable de plantilla
+    #queryset = Book.objects.filter(title__icontains='war')[:5] # Consigue 5 libros que contengan el título de guerra.
+    template_name = 'books/my_arbitrary_template_name_list.html'  # Especifique su propio nombre/ubicación de plantilla
+
+    def get_context_data(self, **kwargs):
+        # Llame primero a la implementación base para obtener un contexto.
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Obtenga el blog del id y agréguelo al contexto.
+        context['some_data'] = 'Estos son solo algunos datos'
+        return context
